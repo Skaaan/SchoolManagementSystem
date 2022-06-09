939a02d22cb436569312e5c8377f1ef0aceec232
Skaaan <104172763+Skaaan@users.noreply.github.com>
2022-06-09 23:28:08 +0100

    Add files via upload

---
+++ filter.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+
+    function addRemoveClass(theRows) {
+
+        theRows.removeClass("odd even");
+        theRows.filter(":odd").addClass("odd");
+        theRows.filter(":even").addClass("even");
+    }
+
+    var rows = $("table#myTable tr:not(:first-child)");
+
+    addRemoveClass(rows);
+
+
+    $("#selectField").on("change", function () {
+
+        var selected = this.value;
+
+        if (selected != "All") {
+
+            rows.filter("[position=" + selected + "]").show();
+            rows.not("[position=" + selected + "]").hide();
+            var visibleRows = rows.filter("[position=" + selected + "]");
+            addRemoveClass(visibleRows);
+        } else {
+
+            rows.show();
+            addRemoveClass(rows);
+
+        }
+    });
+
+
+    $("#selectSemester").on("change", function () {
+
+        var selected = this.value;
+
+        if (selected != "All") {
+
+            rows.filter("[semester=" + selected + "]").show();
+            rows.not("[semester=" + selected + "]").hide();
+            var visibleRows = rows.filter("[semester=" + selected + "]");
+            addRemoveClass(visibleRows);
+        } else {
+
+            rows.show();
+            addRemoveClass(rows);
+
+        }
+    });
+
+
+
+});
